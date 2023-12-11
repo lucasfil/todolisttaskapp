c1420e01fca3f8ab6c1f082dd8198c1ca5c5cec2
Lucas <lucas_j_filho@hotmail.com>
2023-12-11 09:40:10 -0300

    Primeiro commit

---
+++ src/components/TodoWrapper.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import TodoForm from './TodoForm';
+import { v4 as uuidv4 } from 'uuid';
+uuidv4();
+const TodoWrapper = () => {
+
+  const [ todos, setTodos ] = useState([]);
+
+  const addTodo = todo =>{
+    setTodos([...todos, {id:uuidv4(), task:todo, completed:false, isEditing:false}]);
+  }
+  return (
+    <div className="TodoWrapper">
+      <TodoForm addTodo={addTodo}/>
+    </div>
+  )
+}
+
+export default TodoWrapper
